9a103fa6de131f84761d1b3bbac564a21287edd2
himaniman <himaniman@mail.ru>
2023-08-09 16:19:14 +0700

    Add some example variant

--- Example/MainWindow.xaml.cs
+++ Example/MainWindow.xaml.cs
@@ -24,118 +24,95 @@ namespace Example
             var now = new DateTime(2023, 08, 20, 23, 50, 00);
             Items.Add(Guid.NewGuid(), new TimelinerNet.TimelinerItem()
             {
-                Name = "Item",
+                Name = "BUS 33-8479\nSEAT 40",
                 Jobs = new Dictionary<Guid, TimelinerNet.TimelinerJob>()
                 {
                     { Guid.NewGuid(), new TimelinerNet.TimelinerJob()
                         {
-                            Name = "Job",
+                            Name = "P/U GUIDE EO 2484",
+                            TextUp = "Transfer",
                             Begin = now - TimeSpan.FromMinutes(30),
                             End = now - TimeSpan.FromMinutes(15),
+                            Color = Brushes.LightSalmon,
+                            CustomObject = new { CustomString = "Driver: MR. U THAI\n30 km" }
+                        }
+                    },
+                    { Guid.NewGuid(), new TimelinerNet.TimelinerJob()
+                        {
+                            Name = "TROPICAL PARADISE(JUNGLE KINGDOM) (PHUKET)",
+                            TextUp = "Excursion",
+                            Begin = now - TimeSpan.FromMinutes(10),
+                            End = now + TimeSpan.FromMinutes(10),
+                            Color = Brushes.LightPink,
+                            CustomObject = new { CustomString = "Driver: MR. DHET\n120 km" }
+                        }
+                    },
+                    { Guid.NewGuid(), new TimelinerNet.TimelinerJob()
+                        {
+                            Name = "ALL DAY CITY TOUR",
+                            TextUp = "Tour",
+                            Begin = now + TimeSpan.FromMinutes(20),
+                            End = now + TimeSpan.FromMinutes(40),
                             Color = Brushes.LightGreen,
-                            CustomObject = new { CustomString = "custom template\nnew string for template" }
+                            CustomObject = new { CustomString = "Driver: MR. U THAI\n20 km" }
                         }
                     },
                     { Guid.NewGuid(), new TimelinerNet.TimelinerJob()
                         {
-                            Name = "Job2",
-                            Begin = now - TimeSpan.FromMinutes(45),
-                            End = now - TimeSpan.FromMinutes(35)
+                            Name = "M",
+                            TextUp = "Maintance",
+                            Begin = now + TimeSpan.FromMinutes(70),
+                            End = now + TimeSpan.FromMinutes(100),
+                            Color = Brushes.LightYellow,
+                            CustomObject = new { CustomString = "---" }
+                        }
+                    },
+                }
+            });
+            Items.Add(Guid.NewGuid(), new TimelinerNet.TimelinerItem()
+            {
+                Name = "Test item",
+                Jobs = new Dictionary<Guid, TimelinerNet.TimelinerJob>()
+                {
+                    { Guid.NewGuid(), new TimelinerNet.TimelinerJob()
+                        {
+                            Name = "JOB 1",
+                            Begin = now - TimeSpan.FromMinutes(60),
+                            End = now - TimeSpan.FromMinutes(50)
                         }
                     },
                     { Guid.NewGuid(), new TimelinerNet.TimelinerJob()
                         {
-                            Name = "Job3",
-                            Begin = now - TimeSpan.FromMinutes(10),
-                            End = now + TimeSpan.FromMinutes(35),
+                            Name = "Micro",
+                            Begin = now - TimeSpan.FromMinutes(48),
+                            End = now - TimeSpan.FromMinutes(47)
+                        }
+                    },
+                    { Guid.NewGuid(), new TimelinerNet.TimelinerJob()
+                        {
+                            Name = "Colored",
+                            Begin = now - TimeSpan.FromMinutes(40),
+                            End = now - TimeSpan.FromMinutes(30),
+                            Color = Brushes.LightCoral,
+                        }
+                    },
+                    { Guid.NewGuid(), new TimelinerNet.TimelinerJob()
+                        {
+                            Name = "Striped",
+                            Begin = now - TimeSpan.FromMinutes(20),
+                            End = now - TimeSpan.FromMinutes(10),
                             IsStripedColor = true,
-                            Color = Brushes.DarkRed,
-                        }
-                    }
-                }
-            });
-            Items.Add(Guid.NewGuid(), new TimelinerNet.TimelinerItem()
-            {
-                Name = "Item2",
-                Jobs = new Dictionary<Guid, TimelinerNet.TimelinerJob>()
-                {
-                    { Guid.NewGuid(), new TimelinerNet.TimelinerJob()
-                        {
-                            Name = "Job123",
-                            Begin = now - TimeSpan.FromMinutes(20),
-                            End = now - TimeSpan.FromMinutes(18)
+                            Color = Brushes.LightPink,
                         }
                     },
                     { Guid.NewGuid(), new TimelinerNet.TimelinerJob()
                         {
-                            Name = "Job456",
-                            Begin = now - TimeSpan.FromMinutes(10),
-                            End = now + TimeSpan.FromMinutes(10)
+                            Name = "Popup info",
+                            Begin = now - TimeSpan.FromMinutes(9),
+                            End = now - TimeSpan.FromMinutes(0),
+                            CustomObject = new { CustomString = "Additional info" }
                         }
                     },
-                    { Guid.NewGuid(), new TimelinerNet.TimelinerJob()
-                        {
-                            Name = "Micro",
-                            Begin = now - TimeSpan.FromMinutes(30.1),
-                            End = now - TimeSpan.FromMinutes(30)
-                        }
-                    }
-                }
-            });
-            Items.Add(Guid.NewGuid(), new TimelinerNet.TimelinerItem()
-            {
-                Name = "Item3",
-                Jobs = new Dictionary<Guid, TimelinerNet.TimelinerJob>()
-                {
-                    { Guid.NewGuid(), new TimelinerNet.TimelinerJob()
-                        {
-                            Name = "Job123",
-                            Begin = now - TimeSpan.FromMinutes(20),
-                            End = now - TimeSpan.FromMinutes(18)
-                        }
-                    },
-                    { Guid.NewGuid(), new TimelinerNet.TimelinerJob()
-                        {
-                            Name = "Job456",
-                            Begin = now - TimeSpan.FromMinutes(10),
-                            End = now + TimeSpan.FromMinutes(10)
-                        }
-                    },
-                    { Guid.NewGuid(), new TimelinerNet.TimelinerJob()
-                        {
-                            Name = "Micro",
-                            Begin = now - TimeSpan.FromMinutes(30.1),
-                            End = now - TimeSpan.FromMinutes(30)
-                        }
-                    }
-                }
-            });
-            Items.Add(Guid.NewGuid(), new TimelinerNet.TimelinerItem()
-            {
-                Name = "Item4",
-                Jobs = new Dictionary<Guid, TimelinerNet.TimelinerJob>()
-                {
-                    { Guid.NewGuid(), new TimelinerNet.TimelinerJob()
-                        {
-                            Name = "Job123",
-                            Begin = now - TimeSpan.FromMinutes(20),
-                            End = now - TimeSpan.FromMinutes(18)
-                        }
-                    },
-                    { Guid.NewGuid(), new TimelinerNet.TimelinerJob()
-                        {
-                            Name = "Job456",
-                            Begin = now - TimeSpan.FromMinutes(10),
-                            End = now + TimeSpan.FromMinutes(10)
-                        }
-                    },
-                    { Guid.NewGuid(), new TimelinerNet.TimelinerJob()
-                        {
-                            Name = "Micro",
-                            Begin = now - TimeSpan.FromMinutes(30.1),
-                            End = now - TimeSpan.FromMinutes(30)
-                        }
-                    }
                 }
             });
             DataContext = this;
