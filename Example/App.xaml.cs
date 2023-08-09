4d42e7004b3711b354be584dee74876771c49a45
himaniman <himaniman@mail.ru>
2023-08-09 17:21:18 +0700

    Catch unhandled exception

--- Example/App.xaml.cs
+++ Example/App.xaml.cs
@@ -8,10 +8,13 @@ using System.Windows;
 
 namespace Example
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show("An unhandled exception just occurred: " + e.Exception.Message, "Exception Sample", MessageBoxButton.OK, MessageBoxImage.Error);
+            e.Handled = true;
+            App.Current.Shutdown(1);
+        }
     }
 }
